4565f7a9d021188877d162854ccd3bb6aebe133f
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-14 23:17:08 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/resource/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/PostResource.java
@@ -1,26 +1,18 @@
 package com.nelioalves.workshopmongo.resource;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
-import com.nelioalves.workshopmongo.dto.UserDTO;
+import com.nelioalves.workshopmongo.resource.util.URL;
 import com.nelioalves.workshopmongo.services.PostService;
-import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -36,6 +28,15 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 				
 	}
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findbyTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+				
+	}
+	
 	
 	
 
