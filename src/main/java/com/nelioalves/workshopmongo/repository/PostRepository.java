5cd08fde7a65d0051ec38036112628e841b50f89
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-15 00:12:48 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,4 +14,11 @@ public interface PostRepository extends MongoRepository<Post, String>  {
 	
 	@Query("{ 'title': {$regex: ?0, $options: 'i'}}")
 	List<Post> searchTitle(String text);
+	
+	// ?0 = primeiro parametro, ?1 = segundo parametro, ...
+	
+	@Query("{$and: [ {date: {$gte: ?1}}, {date: {$lte: ?2}},"
+			+ " {$or: [ { 'title': {$regex: ?0, $options: 'i'}} ,"
+			+ " { 'body': {$regex: ?0, $options: 'i'}}, { 'comments.text': {$regex: ?0, $options: 'i'}} ]} ] }")
+	List<Post> fullSearch(String text, LocalDate minDate, LocalDate maxDate);
 }
--- src/main/java/com/nelioalves/workshopmongo/resource/util/URL.java
+++ src/main/java/com/nelioalves/workshopmongo/resource/util/URL.java
@@ -2,6 +2,9 @@ package com.nelioalves.workshopmongo.resource.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class URL {
 	
@@ -11,6 +14,17 @@ public class URL {
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+	
+	public static LocalDate convertDate(String textdate, LocalDate defaultValue) {
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		try {
+			return LocalDate.parse(textdate, fmt1);
+		} catch (DateTimeParseException e) {
+			return defaultValue;
+		}
+		
+		
 	}
 
 }
